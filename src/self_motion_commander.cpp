e2da09743d3d82201260c26f5b74b7bfd7063527
thejose5 <thejusjose@outlook.com>
2020-08-18 11:12:37 -0400

    Init position error solved and reduced duration to half

--- src/self_motion_commander.cpp
+++ src/self_motion_commander.cpp
@@ -9,7 +9,7 @@
 
 //Global variables
 Eigen::Matrix<double,6,7> calcJacobian(tf::TransformListener &listener);
-sensor_msgs::JointState joint_state;
+sensor_msgs::JointState joint_state, empty_msg;
 double pi = 3.14159265359;
 
 
@@ -17,8 +17,9 @@ double pi = 3.14159265359;
 
 void generatePoints(trajectory_msgs::JointTrajectory &traj_command, int num_pts,
                           double joint_vels[7], std::vector<double> init_jpos){
-
+  double time_multiplier = 0.5;
   for(int i=0; i<num_pts-1; i++){
+    double time = (i+1)*time_multiplier;
     trajectory_msgs::JointTrajectoryPoint point;
     traj_command.points.push_back(point);
     traj_command.points[i].velocities.push_back(joint_vels[0]);
@@ -29,18 +30,19 @@ void generatePoints(trajectory_msgs::JointTrajectory &traj_command, int num_pts,
     traj_command.points[i].velocities.push_back(joint_vels[5]);
     traj_command.points[i].velocities.push_back(joint_vels[6]);
 
-    traj_command.points[i].positions.push_back(init_jpos[0]+joint_vels[0]*(i+1));
-    traj_command.points[i].positions.push_back(init_jpos[1]+joint_vels[1]*(i+1));
-    traj_command.points[i].positions.push_back(init_jpos[2]+joint_vels[2]*(i+1));
-    traj_command.points[i].positions.push_back(init_jpos[3]+joint_vels[3]*(i+1));
-    traj_command.points[i].positions.push_back(init_jpos[4]+joint_vels[4]*(i+1));
-    traj_command.points[i].positions.push_back(init_jpos[5]+joint_vels[5]*(i+1));
-    traj_command.points[i].positions.push_back(init_jpos[6]+joint_vels[6]*(i+1));
+    traj_command.points[i].positions.push_back(init_jpos[0]+joint_vels[0]*time);
+    traj_command.points[i].positions.push_back(init_jpos[1]+joint_vels[1]*time);
+    traj_command.points[i].positions.push_back(init_jpos[2]+joint_vels[2]*time);
+    traj_command.points[i].positions.push_back(init_jpos[3]+joint_vels[3]*time);
+    traj_command.points[i].positions.push_back(init_jpos[4]+joint_vels[4]*time);
+    traj_command.points[i].positions.push_back(init_jpos[5]+joint_vels[5]*time);
+    traj_command.points[i].positions.push_back(init_jpos[6]+joint_vels[6]*time);
 
-    ros::Duration d(i+1);
+    ros::Duration d(time);
     traj_command.points[i].time_from_start = d;
   }
 
+  double time = (num_pts)*time_multiplier;
   trajectory_msgs::JointTrajectoryPoint point;
   traj_command.points.push_back(point);
 
@@ -52,83 +54,95 @@ void generatePoints(trajectory_msgs::JointTrajectory &traj_command, int num_pts,
   traj_command.points[num_pts-1].velocities.push_back(0);
   traj_command.points[num_pts-1].velocities.push_back(0);
 
-  traj_command.points[num_pts-1].positions.push_back(init_jpos[0]+joint_vels[0]*(num_pts));
-  traj_command.points[num_pts-1].positions.push_back(init_jpos[1]+joint_vels[1]*(num_pts));
-  traj_command.points[num_pts-1].positions.push_back(init_jpos[2]+joint_vels[2]*(num_pts));
-  traj_command.points[num_pts-1].positions.push_back(init_jpos[3]+joint_vels[3]*(num_pts));
-  traj_command.points[num_pts-1].positions.push_back(init_jpos[4]+joint_vels[4]*(num_pts));
-  traj_command.points[num_pts-1].positions.push_back(init_jpos[5]+joint_vels[5]*(num_pts));
-  traj_command.points[num_pts-1].positions.push_back(init_jpos[6]+joint_vels[6]*(num_pts));
+  traj_command.points[num_pts-1].positions.push_back(traj_command.points[num_pts-2].positions[0]+joint_vels[0]*time_multiplier/2);
+  traj_command.points[num_pts-1].positions.push_back(traj_command.points[num_pts-2].positions[1]+joint_vels[1]*time_multiplier/2);
+  traj_command.points[num_pts-1].positions.push_back(traj_command.points[num_pts-2].positions[2]+joint_vels[2]*time_multiplier/2);
+  traj_command.points[num_pts-1].positions.push_back(traj_command.points[num_pts-2].positions[3]+joint_vels[3]*time_multiplier/2);
+  traj_command.points[num_pts-1].positions.push_back(traj_command.points[num_pts-2].positions[4]+joint_vels[4]*time_multiplier/2);
+  traj_command.points[num_pts-1].positions.push_back(traj_command.points[num_pts-2].positions[5]+joint_vels[5]*time_multiplier/2);
+  traj_command.points[num_pts-1].positions.push_back(traj_command.points[num_pts-2].positions[6]+joint_vels[6]*time_multiplier/2);
 
-  ros::Duration d(num_pts);
+  // traj_command.points[num_pts-1].positions.push_back(init_jpos[0]+joint_vels[0]*(num_pts));
+  // traj_command.points[num_pts-1].positions.push_back(init_jpos[1]+joint_vels[1]*(num_pts));
+  // traj_command.points[num_pts-1].positions.push_back(init_jpos[2]+joint_vels[2]*(num_pts));
+  // traj_command.points[num_pts-1].positions.push_back(init_jpos[3]+joint_vels[3]*(num_pts));
+  // traj_command.points[num_pts-1].positions.push_back(init_jpos[4]+joint_vels[4]*(num_pts));
+  // traj_command.points[num_pts-1].positions.push_back(init_jpos[5]+joint_vels[5]*(num_pts));
+  // traj_command.points[num_pts-1].positions.push_back(init_jpos[6]+joint_vels[6]*(num_pts));
+
+  ros::Duration d(time);
   traj_command.points[num_pts-1].time_from_start = d;
 }
 
-// void initializeJState(const sensor_msgs::JointState::ConstPtr &msg){
-//   joint_state = *msg;
-// }
 
 
 
 
-
-
-
-std::vector<double> findInitJPos(tf::TransformListener &listener){
-  std::cout<<std::endl<<"Init Joint Pos:"<<std::endl;
-  std::vector<double> init_jpos;
-  int njoints = 7;
-  tf::StampedTransform transform;
-  double angle;
-
-  //*********************For Joint 0:
-  while(true){
-    try{
-      listener.lookupTransform("/calib_kuka_arm_base_link","/kuka_arm_1_link",
-                             ros::Time(0), transform);
-      break;
-       }
-    catch (tf::TransformException &ex){
-       ROS_ERROR("%s \nTrying Again.",ex.what());
-       ros::Duration(1.0).sleep();
-       continue;
-     }
-  }
-  angle = transform.getRotation().getAngle();
-  Eigen::Vector3d ones(1,1,1);
-  Eigen::Vector3d axis(transform.getRotation().getAxis().x(),
-                       transform.getRotation().getAxis().y(),
-                       transform.getRotation().getAxis().z());
-  std::cout<<"Axis: \n"<<axis<<std::endl<<std::endl;
-  angle = angle*(abs(axis.dot(ones))/axis.dot(ones));
-  std::cout<<angle<<std::endl;
-  init_jpos.push_back(angle);
-  //**************************************
-
-  for(int i=2; i<=njoints; i++){
-    std::string basis_link = "/kuka_arm_"+std::to_string(i-1)+"_link";
-    std::string target_link = "/kuka_arm_"+std::to_string(i)+"_link";
-    try{
-      listener.lookupTransform(basis_link,target_link, ros::Time(0), transform);
-    }
-    catch (tf::TransformException &ex){
-       ROS_ERROR("%s \nTrying Again.",ex.what());
-       ros::Duration(1.0).sleep();
-       --i;
-       continue;
-    }
-    angle = transform.getRotation().getAngle();
-    Eigen::Vector3d ones(1,1,1);
-    Eigen::Vector3d axis(transform.getRotation().getAxis().x(),
-                         transform.getRotation().getAxis().y(),
-                         transform.getRotation().getAxis().z());
-    std::cout<<"Axis: \n"<<axis<<std::endl<<std::endl;
-    angle = angle*(abs(axis.dot(ones))/axis.dot(ones));
-    std::cout<<angle<<std::endl;
-    init_jpos.push_back(angle);
-  }
-  return init_jpos;
+void initializeJState(const sensor_msgs::JointState::ConstPtr &msg){
+  joint_state = *msg;
 }
+
+
+
+
+
+
+
+// std::vector<double> findInitJPos(tf::TransformListener &listener){
+//   std::cout<<std::endl<<"Init Joint Pos:"<<std::endl;
+//   std::vector<double> init_jpos;
+//   int njoints = 7;
+//   tf::StampedTransform transform;
+//   double angle;
+//
+//   //*********************For Joint 0:
+//   while(true){
+//     try{
+//       listener.lookupTransform("/calib_kuka_arm_base_link","/kuka_arm_1_link",
+//                              ros::Time(0), transform);
+//       break;
+//        }
+//     catch (tf::TransformException &ex){
+//        ROS_ERROR("%s \nTrying Again.",ex.what());
+//        ros::Duration(1.0).sleep();
+//        continue;
+//      }
+//   }
+//   angle = transform.getRotation().getAngle();
+//   Eigen::Vector3d ones(1,1,1);
+//   Eigen::Vector3d axis(transform.getRotation().getAxis().x(),
+//                        transform.getRotation().getAxis().y(),
+//                        transform.getRotation().getAxis().z());
+//   std::cout<<"Axis: \n"<<axis<<std::endl<<std::endl;
+//   angle = angle*(abs(axis.dot(ones))/axis.dot(ones));
+//   std::cout<<angle<<std::endl;
+//   init_jpos.push_back(angle);
+//   //**************************************
+//
+//   for(int i=2; i<=njoints; i++){
+//     std::string basis_link = "/kuka_arm_"+std::to_string(i-1)+"_link";
+//     std::string target_link = "/kuka_arm_"+std::to_string(i)+"_link";
+//     try{
+//       listener.lookupTransform(basis_link,target_link, ros::Time(0), transform);
+//     }
+//     catch (tf::TransformException &ex){
+//        ROS_ERROR("%s \nTrying Again.",ex.what());
+//        ros::Duration(1.0).sleep();
+//        --i;
+//        continue;
+//     }
+//     angle = transform.getRotation().getAngle();
+//     Eigen::Vector3d ones(1,1,1);
+//     Eigen::Vector3d axis(transform.getRotation().getAxis().x(),
+//                          transform.getRotation().getAxis().y(),
+//                          transform.getRotation().getAxis().z());
+//     std::cout<<"Axis: \n"<<axis<<std::endl<<std::endl;
+//     angle = angle*(abs(axis.dot(ones))/axis.dot(ones));
+//     std::cout<<angle<<std::endl;
+//     init_jpos.push_back(angle);
+//   }
+//   return init_jpos;
+// }
 
 
 
@@ -149,12 +163,12 @@ int main(int argc, char **argv)
   tf::TransformListener listener;
   ros::Rate rate(10.0);
 
-  // ros::init(argc, argv, "self_motion_commander");
-  // ros::NodeHandle subs_node;
-  // ros::Subscriber joint_state_sub = subs_node.subscribe("/kuka_lwr/joint_states", 1000, initializeJState);
+  ros::init(argc, argv, "self_motion_commander");
+  ros::NodeHandle subs_node;
+  ros::Subscriber joint_state_sub = subs_node.subscribe("kuka_lwr/joint_states", 0, initializeJState);
 
-
-  ros::Duration(1.0).sleep();
+  while(joint_state == empty_msg)
+    ros::spinOnce();
 
   trajectory_msgs::JointTrajectory traj_command;
   traj_command.joint_names.push_back("kuka_arm_0_joint");
@@ -185,20 +199,20 @@ int main(int argc, char **argv)
   std::cout<<qdot<<std::endl;
   double q_dot[qdot.size()];
   Eigen::Map<Eigen::MatrixXd>( q_dot, qdot.rows(), qdot.cols()) =   qdot; //Converting qdot to q_dot array
-  // for(int i=0; i<6; i++){
-  //   std::cout<<q_dot[i]<<" "<<std::endl;
-  // }
-  std::vector<double> init_jpos = findInitJPos(listener);
+  std::vector<double> init_jpos = joint_state.position;
 
-  // generatePoints(traj_command,8,q_dot,init_jpos);
-  // ros::Rate poll_rate(100);
-  //
-  // while(traj_command_pub.getNumSubscribers() == 0)
-  //   poll_rate.sleep();
-  //
-  // traj_command_pub.publish(traj_command);
-  // ros::spinOnce();
-  // loop_rate.sleep();
+  std::cout<<init_jpos[0]<<" "<<init_jpos[1]<<" "<<init_jpos[2]<<" "<<init_jpos[3]<<" "
+  <<init_jpos[4]<<" "<<init_jpos[5]<<" "<<init_jpos[6]<<"\n";
+
+  generatePoints(traj_command,8,q_dot,init_jpos);
+  ros::Rate poll_rate(100);
+
+  while(traj_command_pub.getNumSubscribers() == 0)
+    poll_rate.sleep();
+
+  traj_command_pub.publish(traj_command);
+  ros::spinOnce();
+  loop_rate.sleep();
 
   return 0;
 }
